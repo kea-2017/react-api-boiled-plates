16eff440efa7e5655f881438d654a328e5c3a3f0
gabriel <777canaan@gmail.com>
2017-08-02 23:29:30 +1200

    api not hit

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import {HashRouter as Router, Route} from 'react-router-dom'
+import Fetch from 'node-fetch'
+import request from 'superagent'
 
 import Home from './Home'
 import Header from './Header'
@@ -10,25 +12,32 @@ import GraphTitle from './Graph_title'
 import GraphList from './Graph_list'
 import WeekGraph from './Week_graph'
 
-const App = () => {
-  return (
-    <div>
-      <Router>
-        <div>
-          <Route exact path='/' component={Home}/>
-          <Route path='/graph' component={Header}/>
-          <Route path='/graph' component={SearchBar}/>
-          <Route exact path='/Graph' component={GraphList}/>
-          <Route exact path='/Graph' component={GraphList}/>
-          <Route exact path='/Graph' component={GraphList}/>
-          <Route exact path='/Graph' component={GraphList}/>
-          <Route exact path='/Graph' component={GraphList}/>
-          <Route exact path='/Graph' component={GraphList}/>
-          <Route path='/Graph_list/:id' component={WeekGraph}/>
-        </div>
-      </Router>
-    </div>
-  )
+export default class App extends React.Component {
+  componentDidMount() {
+    request
+    .get('https://min-api.cryptocompare.com/data/')
+    .end((err, res) => {
+      console.log(res.body)
+    })
+  }
+  render() {
+    return (
+      <div>
+        <Router>
+          <div>
+            <Route exact path='/' component={Home}/>
+            <Route path='/graph' component={Header}/>
+            <Route path='/graph' component={SearchBar}/>
+            <Route exact path='/Graph' component={GraphList}/>
+            <Route exact path='/Graph' component={GraphList}/>
+            <Route exact path='/Graph' component={GraphList}/>
+            <Route exact path='/Graph' component={GraphList}/>
+            <Route exact path='/Graph' component={GraphList}/>
+            <Route exact path='/Graph' component={GraphList}/>
+            <Route path='/Graph_list/:id' component={WeekGraph}/>
+          </div>
+        </Router>
+      </div>
+    )
+  }
 }
-
-export default App
